a0414a0300183d2bbe2d141b6ab63cba0a2a8f2a
Tony Zhang <zhgtony.pro@hotmail.com>
2023-01-10 02:22:41 +0100

    :lipstick css & responsive for createaccount.php and fix css for login.php

--- CreateAccount.php
+++ CreateAccount.php
@@ -8,22 +8,28 @@
     <title>CreateAccount</title>
 </head>
 <body class="bg-[#F5F6F7]">
-<h1>WikiFilm</h1>
-<div>
-    <div>
-        <h2>Create a Account</h2>
-        <form method="POST" >
-            <input type="text" name="firstname" placeholder="First name" required>
-            <input type="text" name="lastname" placeholder="Last name" required>
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="email" name="email" placeholder="email">
-            <input type="password" name="password" placeholder="password">
-            <input type="password" name="password2" placeholder="retype password">
-            <input type="submit" value="Submit" >
-        </form>
+    <h1 class="flex justify-center text-4xl font-bold pt-14">Wikifilms</h1>
+    <div class="flex justify-center items-center my-4 lg:my-5">
+        <div class="shadow-lg flex flex-col justify-end items-center mx-4 my-4 lg:my-12 w-[450px] h-[650px] bg-white rounded-[30px] xl:w-[550px] xl:h-[600px]">
+            <h2 class="flex justify-center text-3xl lg:text-4xl font-bold py-8">Create an account</h2>
+            <form method="POST" name="login">
+                <div class="flex justify-center flex-col lg:flex-row lg:gap-4">
+                    <input class="px-6 py-0 gap-2.5 my-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[180px] xl:mr-4" type="text" name="firstname" placeholder="First name" required>
+                    <input class="px-6 py-0 gap-2.5 my-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[180px] xl:mr-4" type="text" name="lastname" placeholder="Last name" required>
+                </div>
+                <div class="flex flex-col items-center">
+                    <input class="px-6 py-0 gap-2.5 my-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="text" name="username" placeholder="Username" required>
+                    <input class="px-6 py-0 gap-2.5 my-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="email" name="email" placeholder="Email">
+                    <input class="px-6 py-0 gap-2.5 my-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="password" name="password" placeholder="Password">
+                    <input class="px-6 py-0 gap-2.5 mt-2 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="password" name="password2" placeholder="Retype password">   
+                    <input class="justify-center px-6 py-0 gap-2.5 my-8 w-[275px] h-[56px] bg-[#F3EDFB] rounded-[30px] xl:w-[400px] xl:mr-4 shadow-md" type="submit" value="Register" name="Register">
+                </div>
+            </form> 
+        </div>
     </div>
-</div>
-
+    <div class="flex justify-center text-2xl font-bold py-2 lg:pb-7">
+        <a href="login.php">Already a member? Sign in</a>
+    </div>
 </body>
 </html>
 
@@ -45,12 +51,14 @@ if($_POST){
     if($user->verify()){
         $connection = new Connection();
         $result = $connection->insert($user);
-        if ($result){
+        if ($result){ 
             echo 'Great ! We Create a account with your data.';
-
+            
             /*create album 2 default album */
             $connection = new Connection();
             $connection->login($_POST['email'], $_POST['password']);
+            
+            header("location: login.php");
 
             require_once 'album.php';
 
@@ -79,7 +87,7 @@ if($_POST){
 
 
 
-            header("location: login.php");
+            // header("location: login.php");
         }
         else{
             echo 'Database error';
--- login.php
+++ login.php
@@ -12,16 +12,19 @@ session_start();
     <title>Login</title>
 </head>
 <body class="bg-[#F5F6F7]">
-    <h1 class="flex justify-center text-4xl font-bold py-8">Wikifilms</h1>
-    <div class="flex justify-center items-center my-4 lg:my-14">
+    <h1 class="flex justify-center text-4xl font-bold pt-14">Wikifilms</h1>
+    <div class="flex justify-center items-center my-4 lg:my-5">
         <div class="shadow-lg flex flex-col justify-end items-center mx-4 my-4 lg:my-12 w-[450px] h-[450px] bg-white rounded-[30px] xl:w-[550px] xl:h-[480px]">
-            <h2 class="flex justify-center text-4xl font-bold py-8">Log In</h2>
+            <h2 class="flex justify-center text-4xl font-bold py-8">Login</h2>
             <form method="POST" name="login">
-                <input class="flex justify-center px-6 py-0 gap-2.5 my-4 w-[275px] h-[56px] bg-[#F3EDFB] rounded-[30px] xl:w-[400px] xl:mr-4" type="email" name="email" placeholder="Email">
-                <input class="flex justify-center px-6 py-0 gap-2.5 w-[275px] h-[56px] bg-[#F3EDFB] rounded-[30px] xl:w-[400px] xl:mr-4" type="password" name="password" placeholder="password">   
-                <input class="flex justify-center px-6 py-0 gap-2.5 my-16 w-[275px] h-[56px] bg-[#F3EDFB] rounded-[30px] xl:w-[400px] xl:mr-4 shadow-md" type="submit" value="Submit" name="submit">
+                <input class="flex justify-center px-6 py-0 gap-2.5 my-4 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="email" name="email" placeholder="Email">
+                <input class="flex justify-center px-6 py-0 gap-2.5 w-[275px] h-[56px] border-[#333] border-2 rounded-[30px] xl:w-[400px] xl:mr-4" type="password" name="password" placeholder="password">   
+                <input class="flex justify-center px-6 py-0 gap-2.5 my-16 w-[275px] h-[56px] bg-[#F3EDFB] rounded-[30px] xl:w-[400px] xl:mr-4 shadow-md" type="submit" value="Sign in" name="Sign in">
             </form> 
         </div>
+    </div>
+    <div class="flex justify-center text-2xl font-bold py-2 lg:pb-7">
+        <a href="CreateAccount.php">Not a member? Sign up now</a>
     </div>
 </body>
 </html>
@@ -41,6 +44,6 @@ if($_POST){
         header("Location: index.php");
         echo "utilisateur trouvé";
     } else { ?>
-    <h3 class="flex justify-center text-2xl font-bold py-2 lg:py-0">Email or password not valid</h3>
+    <h3 class="flex justify-center text-xl lg:text-2xl font-bold py-2 lg:py-0">Email or password not valid</h3>
     <?php }
 } ?>
